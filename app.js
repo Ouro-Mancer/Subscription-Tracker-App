15c8904637537dee9eea86370e76642e6125b0b0
Mayank <mayankkumardas12@gmail.com>
2025-06-18 13:53:31 +0530

    middleware done

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import { PORT } from "./config/env.js";
 
 import userRouter from "./routes/userRoutes.js";
 import authRouter from "./routes/authRoutes.js";
 import subscriptionRouter from "./routes/subscriptionRoutes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middleware/errorMiddleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Subscription Tracker API!");
